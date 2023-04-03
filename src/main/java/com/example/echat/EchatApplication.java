7f3a07505f4180c8dbdea2e8c358d447c7b09bc5
danny309 <danny.1988@163.com>
2023-04-03 10:48:12 +0800

    44

---
+++ src/main/java/com/example/echat/EchatApplication.java
@@ -0,0 +1,13 @@
+package com.example.echat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/echat/EchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.echat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
